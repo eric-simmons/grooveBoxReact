9bbe4978e71aeab2d703beb6d997b7558e3a1d8c
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-23 18:31:42 -0600

    selecting steps

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,17 +1,35 @@
 import React from "react"
 
-let steps = []
-
-for (let i = 0; i < 16; i++) {
-    steps.push({
+let steps = [...Array(16).keys()]
+steps.map((step, index) => {
+   return steps[index] = ({
+        stepId: Math.random(),
         noteOn: false,
         velocity: 0,
         playhead: false,
         firstStep: false,
         lastStep: false,
+        isDown: false
+    })
+})
+// let patternLength = 16
+
+
+console.log(steps)
+
+//change to map
+for (let i = 0; i < steps.length; i++) {
+    steps[i] = ({
+        stepId: Math.random(),
+        noteOn: false,
+        velocity: 0,
+        playhead: false,
+        firstStep: false,
+        lastStep: false,
+        isDown: false
     })
 }
-
+console.log(steps)
 
 
 class Grid extends React.Component {
@@ -20,25 +38,40 @@ class Grid extends React.Component {
         this.state = {
             steps: steps
         }
-        this.activateStep = this.activateStep.bind(this)
+        this.toggleStep = this.toggleStep.bind(this)
     }
 
-    activateStep() {
-        let steps = this.state.steps
-        this.setState({ steps: steps });
+    toggleStep(stepId) {
+        console.log(stepId)
+        const updatedSteps = [...this.state.steps].map(step => {
+            return {
+                ...step,
+                noteOn : step.stepId === stepId ? !step.noteOn : step.noteOn
+            }
+        })
+        //in updated steps find step by id
+
+        //modify step object
+
+        console.log(updatedSteps)
+        this.setState({
+            steps: updatedSteps
+        })
     }
 
 
     render() {
         return (
             <div className="grid">
-                {this.state.steps.map((button, i) => {
-                    console.log(button)
+                {this.state.steps.map((step, i) => {
                     return (
                         <button className='stepBtn'
-                            key={'step' + i}
-                            onClick={this.activateStep}
-                        />
+                            key={i}
+                            onClick={() => this.toggleStep(step.stepId)}
+                            style={{ backgroundColor: step.noteOn ? 'red' : 'blue' }}
+                        >
+                            {step.noteOn ? 'Active' : 'Inactive'}
+                        </button>
                     )
                 })}
             </div>
